0f09f9dbe4fd280ef5d7b40fcd8d63b7032c3969
boubaker <boubakerboulaid205@gmail.com>
2026-04-07 15:35:36 +0100

    feat: replace welcome route with MessageController index and send endpoints

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MessageController::class, 'index']);
+Route::post('/send', [MessageController::class, 'send']);
