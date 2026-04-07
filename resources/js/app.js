2f751ac8bb4b58925447aae022c3cc4f5c8746be
boubaker <boubakerboulaid205@gmail.com>
2026-04-07 16:25:54 +0100

    feat: implement ShouldBroadcast on MessageSentEvent and update Echo listener to match event name

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import "./bootstrap";
-window.Echo.channel("chat").listen("MessageSent", (e) => {
+window.Echo.channel("chat").listen("MessageSentEvent", (e) => {
+    console.log("message received:", e);
     let div = document.getElementById("messages");
     div.innerHTML += `<p>${e.message}</p>`;
 });
